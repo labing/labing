270cc3d5ecff11b237273e9cc6ba974f06fc51f0
ubuntu <xogns824@nate.com>
2015-05-29 14:04:32 +0900

    lth

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ActiveRecord::Base
   belongs_to :bulletin
   has_many :comments, dependent: :destroy
   mount_uploader :picture, PictureUploader
+  validates :content, :presence => { :message => "Please write something."}
 end
